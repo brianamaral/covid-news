255389d1090c342591bb8e0d760e2a7c398cbd1a
brianamaral <brianamaral918@gmail.com>
2021-06-18 13:52:35 -0300

    changed the db from mongo for postgres

--- webscraper/first_page.py
+++ webscraper/first_page.py
@@ -1,25 +1,39 @@
 from scrap import Scraper
-from pymongo import MongoClient
+from sqlalchemy import create_engine
+import pandas as pd
 from tqdm import tqdm
+from schedule import every, repeat, run_pending
+import time
 
-#change for your mongodb credentials
-MONGO_CONNECTION = ''
-MONGO_PORT = 27017
-MONGO_USER = ''
-MONGO_PASSWORD = ''
-MONGO_DB = 'test'
+#change for your POSTGRES credentials
+POSTGRES_USER = ''
+POSTGRES_PASSWORD = ''
+POSTGRES_ADDRES = ''
+POSTGRES_DATABASE = ''
 
-connection = MongoClient(MONGO_CONNECTION,MONGO_PORT)
-database = connection[MONGO_DB]
+engine = create_engine('postgresql+psycopg2://{}:{}@{}/{}'.format(POSTGRES_USER,POSTGRES_PASSWORD,
+                                                                  POSTGRES_ADDRES,POSTGRES_DATABASE))
+connection = engine.connect()
 
 scrap = Scraper()
 scrap.get_links(1)
 
-for link in tqdm(scrap.links):
-    url = scrap.base_url + link
+df_articles = pd.DataFrame(columns=['title','date','text','url','author'])
 
-    if database.col.find_one({'Url':url}) == None:
+@repeat(every().day.at('12:00'),scrap,df_articles)
+def scraped_page_to_postgres(scrap,df_articles):
+    for link in tqdm(scrap.links):
+        url = scrap.base_url + link
+
         article = scrap.scrap_article(link)
-        database.col.insert_one(article)
-        
-    
+
+        article_row = {'title':article['Titulo'],'date':article['Data'],
+                   'text':article['Texto'],'url':article['Url'],
+                   'author':article['Autor']}
+        df_articles = df_articles.append(article_row,ignore_index=True)
+
+    df_articles.to_sql('articles',connection,if_exists='append',index=False)       
+
+while True:
+    run_pending()
+    time.sleep(1)
